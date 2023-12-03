9797aaab285ecf5dd1f677bc65e757a003d62111
JoeyZhang00111 <144625929+JoeyZhang00111@users.noreply.github.com>
2023-12-03 11:45:21 -0600

    Update main.java
    it took me a while to figure out how to change it

--- main.java
+++ main.java
@@ -1,6 +1,13 @@
 
 public class main {
 	public static void main(String args[]){
-		
+		Employee emp1 = new Employee("John Doe");
+        System.out.println("Employee Name: " + emp1.getName());
+        System.out.println("Employee Number: " + emp1.getEmployeeNumber());
+
+        Employee emp2 = new Employee("Jane Smith");
+        System.out.println("Employee Name: " + emp2.getName());
+        System.out.println("Employee Number: " + emp2.getEmployeeNumber());
+
 	}
 }
