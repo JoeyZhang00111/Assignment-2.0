805f71901e15397c54097ac35a9c83b8292c7a9c
JoeyZhang00111 <144625929+JoeyZhang00111@users.noreply.github.com>
2023-12-20 20:27:03 -0600

    Updated final main.java

--- main.java
+++ main.java
@@ -3,10 +3,19 @@ public class main {
 	static final int START_EMPLOYEE_NUMBER = 1000;
 	
 	public static void main(String[] args) {
-		Employee emp1 = new Employee(20, 10, START_EMPLOYEE_NUMBER); 
-        Employee emp2 = new Employee(15, 8, emp1.getNextEmployeeNumber()); 
-//        emp2.setHoursWorked(50);
-//        emp2.setHourlyWage(15.3);
+		Employee emp1 = new Employee(0, 0, START_EMPLOYEE_NUMBER); 
+        Employee emp2 = new Employee(0, 0, emp1.getNextEmployeeNumber()); 
+        Employee emp3 = new Employee(0, 0, emp1.getNextEmployeeNumber());
+        
+        emp1.setHoursWorked(10);
+        emp1.setHourlyWage(20);
+        
+        emp2.setHoursWorked(8);
+        emp2.setHourlyWage(15);
+        
+        emp3.setHoursWorked(9);
+        emp3.setHourlyWage(18);
+        
 
         
 		
@@ -15,12 +24,31 @@ public class main {
 
         System.out.println("\nEmployee 2 Record:");
         System.out.println(emp2);
+        
+        System.out.println("\nEmployee 3 Record:");
+        System.out.println(emp3);
 
-        // Demonstrating equals method
-        System.out.println("\nEquality Check:");
-        System.out.println("Employee 1 equals Employee 2: ");
-        System.out.println("Employee 1 equals Employee 1: ");
-
-       
+        
+        
+        System.out.println("\nDemonstrating equals method:");
+        System.out.println("Employee 1 equals Employee 2: " + emp1.equals(emp2));
+        System.out.println("Employee 2 equals Employee 3: " + emp2.equals(emp3));
+        
+        
+        
+        System.out.println("\nGetters and setters demonstration :");
+        System.out.println("Previous hourly wage of Employee 1: " + emp1.getHourlyWage());
+        emp1.setHourlyWage(25.0);
+        System.out.println("Updated hourly wage of Employee 1: " + emp2.getHourlyWage());
+        
+        System.out.println("Previous hourly wage of Employee 2: " + emp2.getHourlyWage());
+        emp1.setHourlyWage(25.0);
+        System.out.println("Updated hourly wage of Employee 2: " + emp2.getHourlyWage());
+        
+        System.out.println("Previous hourly wage of Employee 3: " + emp3.getHourlyWage());
+        emp1.setHourlyWage(25.0);
+        System.out.println("Updated hourly wage of Employee 3: " + emp3.getHourlyWage());
+        
     }
 }
+
