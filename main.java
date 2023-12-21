c34cba923d3423fcd6b17c237b3c2b0516b32d09
JoeyZhang00111 <144625929+JoeyZhang00111@users.noreply.github.com>
2023-12-20 19:49:08 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,13 +1,26 @@
-
+import java.util.Random;
 public class main {
-	public static void main(String args[]){
-		Employee emp1 = new Employee("John Doe");
-        System.out.println("Employee Name: " + emp1.getName());
-        System.out.println("Employee Number: " + emp1.getEmployeeNumber());
+	static final int START_EMPLOYEE_NUMBER = 1000;
+	
+	public static void main(String[] args) {
+		Employee emp1 = new Employee(20, 10, START_EMPLOYEE_NUMBER); 
+        Employee emp2 = new Employee(15, 8, emp1.getNextEmployeeNumber()); 
+//        emp2.setHoursWorked(50);
+//        emp2.setHourlyWage(15.3);
 
-        Employee emp2 = new Employee("Jane Smith");
-        System.out.println("Employee Name: " + emp2.getName());
-        System.out.println("Employee Number: " + emp2.getEmployeeNumber());
+        
+		
+        System.out.println("Employee 1 Record:");
+        System.out.println(emp1);
 
-	}
+        System.out.println("\nEmployee 2 Record:");
+        System.out.println(emp2);
+
+        // Demonstrating equals method
+        System.out.println("\nEquality Check:");
+        System.out.println("Employee 1 equals Employee 2: ");
+        System.out.println("Employee 1 equals Employee 1: ");
+
+       
+    }
 }
